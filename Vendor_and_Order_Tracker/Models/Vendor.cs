1cdfffedaac0ea8cf59e24e3584bdfc77d0f10cb
Randel M <Randel.C.Moore@gmail.com>
2020-12-18 14:41:52 -0800

    add contructor to Vendor.cs

--- Vendor_and_Order_Tracker/Models/Vendor.cs
+++ Vendor_and_Order_Tracker/Models/Vendor.cs
@@ -4,6 +4,30 @@ namespace Vendor_and_Order_Tracker.Models
 {
   public class Vendor
   {
-    
+    private static List<Vendor> _vendorList = new List<Vendor> {};
+
+    private string VendorName { get; set; }
+    private string Id { get; set; }
+    private string ShipToName { get; set; }
+    private string Address { get; set; }
+    private string State { get; set; }
+    private string City { get; set; }
+    private string Zipcode { get; set; }
+    private string Phone { get; set; }
+    private string Email { get; set; }
+
+    public Vendor(string vendorName, string shipTo, string shipAddress, string shipState, string shipCity, string shipZip, string phoneNum, string email)
+    {
+      _vendorList.Add(this);
+      VendorName = vendorName;
+      Id = uniqueVenId();
+      ShipToName = shipTo;
+      Address = shipAddress;
+      State = shipState;
+      City = shipCity;
+      Zipcode = shipZip;
+      Phone = phoneNum;
+      Email = email;
+    }
   }
 }
