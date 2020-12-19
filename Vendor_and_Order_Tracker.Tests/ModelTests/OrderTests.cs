955f7dcd87355d5aeda3e96fa24e643cb5e22105
Randel M <Randel.C.Moore@gmail.com>
2020-12-18 16:45:33 -0800

    pass the order instantiation test

--- Vendor_and_Order_Tracker.Tests/ModelTests/OrderTests.cs
+++ Vendor_and_Order_Tracker.Tests/ModelTests/OrderTests.cs
@@ -17,7 +17,7 @@ namespace Vendor_and_Order_Tracker.Tests
     public void OrderConstructor_CreatesInstanceOfOrder_True()
     {
       Order ord1 = new Order("a","b","c","d","e","f","g","h", "i");
-      Assert.AreEqual(typeof(int), ord1.GetType());
+      Assert.AreEqual(typeof(Order), ord1.GetType());
     }
   }
 }
