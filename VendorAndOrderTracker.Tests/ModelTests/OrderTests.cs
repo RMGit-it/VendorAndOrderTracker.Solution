e8e19dc9808bb03b69a71aa5429bfb6dee490aab
Randel M <Randel.C.Moore@gmail.com>
2020-12-23 05:41:51 -0800

    change filenames to conform to conventions

--- VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -19,7 +19,7 @@ namespace Vendor_and_Order_Tracker.Tests
     public void OrderConstructor_CreatesInstanceOfOrder_True()
     {
       Vendor ven1 = new Vendor("a","b","c","d","e","f","g","h");
-      Order ord1 = new Order(1,"od","dfd",1,1,1,1,1,1);
+      Order ord1 = new Order(1, "txt", "desc", 2, "od","dfd",1,1,1,1,1,1);
       Assert.AreEqual(typeof(Order), ord1.GetType());
     }
 
@@ -27,7 +27,7 @@ namespace Vendor_and_Order_Tracker.Tests
     public void GetVariousOrderProperties_ReturnVariousPropertiesOfOrder_true()
     {
       Vendor ven1 = new Vendor("a","b","c","d","e","f","g","h");
-      Order ord1 = new Order(1,"od","dfd",1,1,1,1,1,1);
+      Order ord1 = new Order(1, "txt", "desc", 2, "od","dfd",1,1,1,1,1,1);
       Assert.AreEqual(1, ord1.GetOrderVendorId());
       Assert.AreEqual("od", ord1.GetOrderDate());
       Assert.AreEqual("dfd", ord1.GetDeliveryForDate());
